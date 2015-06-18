521eb7fb8f3c77ae99636982dc69447ffa5807ca
Vanessa <lynvaness192@gmail.com>
2015-06-17 17:57:49 -0700

    Landing Page added by Vanessa

--- resources/views/landingApp.blade.php
+++ resources/views/landingApp.blade.php
@@ -1,89 +1,223 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Talentscool</title>
-	
-	<link rel="shortcut icon" href="{{{ asset('/images/favicon.jpeg') }}}">
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-	<link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>	<style>
-	body{
-	font-family: 'Open Sans', sans-serif;
-	}
-	</style>
-	
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-
-<ul class="headerWrapper">
-	<li class="image" id="img"><a href="{{action('WelcomeController@index')}}">{!! HTML::image('/images/TSVector.png', null, array('style'=>"width:100%")) !!}</a>
-	<li class="content"><a href="#" onmouseover="mopen('m1')" onmouseout="mclosetime()" id="test"">Company</a>
-		<div id="m1">
-			<a href="{{action('WelcomeController@aboutus')}}" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">About Us</a>
-			<a href="{{action('WelcomeController@whatwedo')}}" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">What We Do</a>
-		</div>
-	</li>
-	<li class="content"><a href="#"  onmouseover="mopen('m2')" onmouseout="mclosetime()" >Company2</a>
-		<div  id="m2">
-			<a href="#" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">test2</a>
-			<a href="#" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">test3</a>
-		</div>
-	</li>
-	<li class="content" style="width:37%; border:none;" >
-		&nbsp;
-	</li>
-</ul>
-	@yield('content')
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	<script>
-	var timeout = 500;
-	var closetimer = 0;
-	var curMenu = 0;
-	function mopen(id)
-	{
-			mcancelclosetime();
-			if(curMenu)
-				curMenu.style.visibility="hidden";
-			curMenu = document.getElementById(id);
-			curMenu.style.visibility="visible";
-			curMenu.style.display="none";
-			$(curMenu).slideDown();
-	}
-	function mclose()
-	{
-			if(curMenu)
-				$(curMenu).slideUp();
-	}
-	function mclosetime()
-	{
-		closetimer = window.setTimeout(mclose, timeout);
-	}
-	function mcancelclosetime()
-	{
-			if(closetimer)
-			{
-					window.clearTimeout(closetimer);
-					closetimer = null;	
-			}
-	}
-	
-	</script>
-	@yield('tail')
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, maximum-scale=1">
+		<title>Talentscool</title>
+		
+		<link rel="shortcut icon" href="http://talentscool.com/images/favicon.jpeg">
+		<link href="css/font-awesome.css" rel="stylesheet" type="text/css"> 
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="css/style7.css" rel="stylesheet" type="text/css"> 
+		<link href="css/animate.css" rel="stylesheet" type="text/css"> 
+		
+		
+		<!-- <link rel="shortcut icon" href="{{{ asset('/images/favicon.jpeg') }}}">
+			<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/style7.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet">
+		<link href="{{ asset('/css/animate.css') }}" rel="stylesheet"> -->
+		
+		
+		<!--[if lt IE 9]>
+			<script src="js/respond-1.1.0.min.js"></script>
+			<script src="js/html5shiv.js"></script>
+			<script src="js/html5element.js"></script>
+		<![endif]-->
+		
+	</head>
+	<body>
+		
+		<!--Header_section-->
+		<header id="header_wrapper">
+			<div class="container">
+				<div class="header_box">
+					<div class="logo"><a href="#"><img src="images/TSVector.png" alt="logo"></a></div>
+					<nav class="navbar navbar-inverse" role="navigation">
+						<div class="navbar-header">
+							<button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+						</div>
+						<div id="main-nav" class="collapse navbar-collapse navStyle">
+							<ul class="nav navbar-nav" id="mainNav">
+								<li class="active"><a href="#hero_section" class="scroll-link">Home</a></li>
+								<li><a href="#aboutUs" class="scroll-link">About Us</a></li>
+								<li><a href="#service" class="scroll-link">Services</a></li>
+								<li><a href="#contact" class="scroll-link">Contact</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<!--Header_section--> 
+		
+		<!--Hero_Section-->
+		<section id="hero_section" class="top_cont_outer">
+			<div class="hero_wrapper">
+				<div class="container">
+					<div class="hero_section">
+						<div class="row">
+							<div class="col-lg-5 col-sm-7">
+								<div class="top_left_cont zoomIn wow animated"> 
+									<h2>We are the new way to connect in Entertainment</h2>
+									<p>Whether you are looking for talent or your next big break, we are here to help you.</p>
+									<p>Music. Dance. Film. Fashion. Arts. Sports. We know them all.</p>
+								<a href="#service" class="read_more2">Read more</a> </div>
+							</div>
+							<div>
+								<img src="images/mainImg.png" class="bannerImg zoomIn wow animated" alt="" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--Hero_Section--> 
+		
+		<!--Aboutus-->
+		<!-- style="background-color: black;" -->
+		<section id="aboutUs" >
+			<div class="inner_wrapper">
+				<div class="container">
+					<h2 id="auhead">About Us</h2>
+					<div class="inner_section">
+						<div class="row">
+							<div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right"><img src="images/about-img.jpg" class="img-circle delay-03s animated wow zoomIn" alt=""></div>
+							<div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left">
+								<div class=" delay-01s animated fadeInDown wow animated">
+									
+									<p>Headquartered in Los Angeles, Talentscool is a talent service that works with talents in music, film, fashion, visual arts, dance, and sports to find them opportunities and connect them to the various services they need for their careers. Being founded by both a musician and a pro-athlete, we know how hard it is to stand out in the entertainment industry. With all of the competition, we aim to give our clients ease knowing someone is on their side.</p> <br/>
+									<p>Through our global relationships with major/indie record labels, talent agencies, casting agencies, production companies, venues, art galleries, corporate sponsors, and much more, we are able to provide the best possible service to our clients. In addition, we recommend our clients to well-established vendor services ranging from publicists to agents to investors to entertainment lawyers and everything in between. We cater to the needs of our clients in every way possible.</p>
+								</div>
+								<div class="work_bottom"> <span>Want to know more..</span> <a href="#contact" class="contact_btn">Contact Us</a> </div>       
+							</div>
+							
+						</div>
+						
+						
+					</div>
+				</div> 
+			</div>
+		</section>
+		<!--Aboutus--> 
+		
+		
+		<!--Service-->
+		<section  id="service" style="font-size: 16px;">
+			<div class="service_wrapper" >
+				<div class="container">
+					<h2>Services</h2>
+					<div class="member wow bounceInUp animated animated" style="visibility: visible; animation-name: bounceInUp; -webkit-animation-name: bounceInUp;">
+						<ul>
+							<li class="listHeader">General</li>
+							<ul>
+								<li>Opportunities, Representation, Branding, Talent Development, Collaboration, Social Media Management</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Opportunities</li>
+							<ul>
+								<li>Bookings, Endorsements, Auditions, Internships, Showcases, Appearances</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Representation</li>
+							<ul>
+								<li>Manager/Agent, Talent Agency, Publicist, Booking Agent</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Outside Services</li>
+							<ul>
+								<li>Musician, Vocalist, Dancer, Consultant, Lawyer, Accountant, Personal Assistant, Fitness Trainer, Video/Film Production, Event Planner, Graphic Designer, Audio Engineer</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Talent Development</li>
+							<ul>
+								<li>Vocal Coach, Acting Coach, Athletic Trainer, Dance Instructor, Instrument Teacher</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Vanity Services</li>
+							<ul>
+								<li>Clothing Stylist, Hair Stylist, Make-Up Artists, Clothing Designer, Photographer, Videographer, Model</li>
+							</ul>
+						</ul>
+						<ul>
+							<li class="listHeader">Luxury Services</li>
+							<ul>
+								<li>Body Guard, Limo Drivers, Private Jet, Car Rental, Studio Rental</li>
+							</ul>
+						</ul>
+						
+						
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--Service-->
+		
+		
+		
+		
+		
+		<!--/Portfolio --> 
+		
+		<!--client_logos-->
+		
+		
+		<!--/Team-->
+		<!--Footer-->
+		<footer class="footer_wrapper" id="contact">
+			<div class="container">
+				<section class="page_section contact" id="contact">
+					<div class="contact_section">
+						<h2>Contact Us</h2>
+						<div class="row">
+							<div class="col-lg-4">
+								
+							</div>
+							<div class="col-lg-4">
+								
+							</div>
+							<div class="col-lg-4">
+								
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						
+						<div class="col-lg-12 wow fadeInLeft delay-06s">
+							<div class="form">
+								<input class="input-text" type="text" name="" value="Your Name *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+								<input class="input-text" type="text" name="" value="Your E-mail *" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">
+								<textarea class="input-text text-area" cols="0" rows="0" onFocus="if(this.value==this.defaultValue)this.value='';" onBlur="if(this.value=='')this.value=this.defaultValue;">Your Message *</textarea>
+								<input class="input-btn" type="submit" value="send message">
+							</div>
+						</div>
+						
+					</div>
+				</section>
+				</div>
+			<!-- <div class="container">
+				<div class="footer_bottom"><img src="http://talentscool.com/images/logoT.png" style="vertical-align:middle; max-height: 50%; max-width: 50%;"> </div>
+				<!-- class="tailLogo" 
+			</div> -->
+		</footer>
+		
+		<!-- Scripts -->
+		<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+		<script type="text/javascript" src="js/jquery.nav.js"></script> 
+		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="js/jquery.isotope.js"></script>
+		<script type="text/javascript" src="js/wow.js"></script> 
+		<script type="text/javascript" src="js/custom.js"></script>
+		
+	</body>
 </html>
