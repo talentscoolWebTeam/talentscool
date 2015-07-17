6f3809e12a2cb8ef911557ce7a4ca1899d3fd395
George Butiri <MovieMaker713@gmail.com>
2015-07-17 16:54:13 -0500

    syncing thanks blade

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Thanks for your application</title>
+<style>
+body{
+	   background: url("http://www.talentscool.com/images/background.png") no-repeat center center fixed; 
+       -webkit-background-size: cover;
+       -moz-background-size: cover;
+       -o-background-size: cover;
+       background-size: cover;	
+	   color:white;
+	   font:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
+	   font-size:20px;
+	   padding-left:5%;
+	}
+	img {
+	float: right top;
+    margin-top:2%;
+	position:relative;
+	margin-left:80%;
+	width:200px;
+	
+}
+</style>
+</head>
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+<body>
+<div class="thanks">
+<img src="/pic/TSVector.png">
+<p style="font-size:30px; position:relative; color:white; margin-top:15%; margin-left:10%; margin-right:15%;">Thanks for your interest. Your application has been submitted.<br>A comfirmation email will be sent to your email address. If there is a match, we will contact you ASAP.<br>Thank you!</p>
+</div>
+</body>
+=======
 <!doctype html>
 <html>
 <head>
@@ -33,4 +69,5 @@ body{
 <p style="font-size:30px; position:relative; color:white; margin-top:15%; margin-left:10%; margin-right:15%;">Thanks for your interest. Your application has been submitted. If there is a match, we will contact you ASAP.<br>Thank you!</p>
 </div>
 </body>
+>>>>>>> 58f0f6caf1a4fc027ed3555721565f4cbbf46597
 </html>
