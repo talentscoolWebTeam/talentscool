8e2e9f1215e4e096041d0fd0d838c4c7dc4bcbfc
Joseph Chen <chenjose@usc.edu>
2015-07-21 16:53:55 -0700

    fixing error blade

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!doctype html>
 <html>
 <head>
@@ -38,46 +37,4 @@ body{
         location.href = "/application";
     };
 </script>
-=======
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Application finished</title>
-<style>
-body{
-	   background: url("http://www.talentscool.com/images/background.png") no-repeat center center fixed; 
-       -webkit-background-size: cover;
-       -moz-background-size: cover;
-       -o-background-size: cover;
-       background-size: cover;	
-	   color:white;
-	   font:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", serif;
-	   font-size:20px;
-	   padding-left:5%;
-	}
-	img {
-	float: right top;
-    margin-top:2%;
-	position:relative;
-	margin-left:80%;
-	width:200px;
-	
-}
-</style>
-</head>
-<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-<body>
-<img src="/images/TSVector.png">
-<div class="error">
-<p style="font-size:30px; position:relative; color:white; margin-top:15%; margin-left:10%; margin-right:15%;">The files you submitted have some problems. Please make sure that the files you submitted have valid extension names and each one is limited to 5MB.<br><input type="button" value="Back to application page" id="myButton" class="btn btn-danger"></input></p>
-
-</div>
-</body>
-<script type="text/javascript">
-    document.getElementById("myButton").onclick = function () {
-        location.href = "http://talentscool.local/application";
-    };
-</script>
->>>>>>> 58f0f6caf1a4fc027ed3555721565f4cbbf46597
 </html>
