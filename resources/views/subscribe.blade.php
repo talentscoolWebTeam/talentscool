e994eb1e65b748d1fad135a313441cc5043ac704
Joseph Chen <chenjose@usc.edu>
2015-07-30 15:32:33 -0700

    adding new subscribe page

---
+++ resources/views/subscribe.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, maximum-scale=1">
+		<title>Subscribe!</title>
+		
+		<link rel="shortcut icon" href="http://talentscool.com/images/favicon.jpeg">
+		<link href="css/font-awesome.css" rel="stylesheet" type="text/css"> 
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<link href="css/style7.css" rel="stylesheet" type="text/css"> 
+		<link href="css/animate.css" rel="stylesheet" type="text/css">
+		
+		
+		<!-- <link rel="shortcut icon" href="{{{ asset('/images/favicon.jpeg') }}}">
+			<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/style7.css') }}" rel="stylesheet">
+			<link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet">
+		<link href="{{ asset('/css/animate.css') }}" rel="stylesheet"> -->
+		
+		
+		<!--[if lt IE 9]>
+			<script src="js/respond-1.1.0.min.js"></script>
+			<script src="js/html5shiv.js"></script>
+			<script src="js/html5element.js"></script>
+		<![endif]-->
+		<style>
+            .hidden{
+                display:hidden;
+            }
+        </style>
+	</head>
+	<body>
+
+	</body>
+</html>
