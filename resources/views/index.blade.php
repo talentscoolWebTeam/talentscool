4af16812d3068dab8ae52cfa772d998a887fd8f8
KNGeeting <geeting@usc.edu>
2016-01-30 10:33:39 -0800

    layout changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="The Entertainment Connection"/>
     <title>TalentsCool</title>
     <link rel="stylesheet" href="css/welcome-style.css" />
     <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
