652f2b5b6345885ccb8c5db97827b8cf56549ddc
Kevin <geeting@usc.edu>
2016-04-09 10:56:51 -0700

    contact form fixed

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,16 @@
+<!--Formatting the emails-->
+
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Contact</title>
+</head>
+<body>
+	<p><strong>Name:</strong>{{$name}}</p>
+	<p><strong>Email address:</strong>{{$email}}</p>
+	<p><strong>Message:</strong>{{$msge}}</p>
+</body>
+
+</html>
